86c68f3cbcc8c8729a377d1787ba7ecf6be0b49c
Aditi <asaki444@gmail.com>
2020-01-25 01:59:01 -0500

    finito

--- index.js
+++ index.js
@@ -1,49 +1,93 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-const days =  ["S", "M", "T", "W", "T", "F", "S"]
 
-function onLoad(){
-    for(let i = 0; i < days.length; i++){
-        if( i === 0 || i === days.length - 1){
-            document.getElementById("days-row").innerHTML+= `<div class="edge-day">${days[i]}</div>`
-        }
-        else{
-            document.getElementById("days-row").innerHTML+= `<div>${days[i]}</div>`
+const days = ["S", "M", "T", "W", "T", "F", "S"]
+
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let currentMonth = currentDate.getMonth();
+
+
+
+function onLoad() {
+
+    document.getElementById("cursor-left").addEventListener('click', () => {
+        scroll('left')
+    });
+    document.getElementById("cursor-right").addEventListener('click', () => {
+        scroll('right')
+    })
+
+    for (let i = 0; i < days.length; i++) {
+        if (i === 0 || i === days.length - 1) {
+            document.getElementById("days-row").innerHTML += `<div class="edge-day">${days[i]}</div>`
+        } else {
+            document.getElementById("days-row").innerHTML += `<div>${days[i]}</div>`
         }
     }
 
 
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth();
+    loadCalendar(currentMonth, currentYear);
+
+}
+
+function scroll(direction) {
+
+    if (direction === 'right') {
+        11 > currentMonth ? currentMonth += 1 : currentMonth = 0;
+        if (currentMonth === 0) {
+            currentYear += 1;
+        }
+    } else {
+        currentMonth > 0 ? currentMonth -= 1 : currentMonth = 11;
+        if (currentMonth === 11) {
+            currentYear -= 1;
+        }
+
+    }
+    document.getElementById("month").innerHTML = "";
+    document.querySelectorAll("#week-row-1, #week-row-2, #week-row-3, #week-row-4, #week-row-5").forEach(element => {
+        element.innerHTML = ""
+    })
+
+
+
+    loadCalendar(currentMonth, currentYear)
+}
+
+
+
+
+
+
+function loadCalendar(month, year) {
+
     let findFirstDay = new Date(year, month, 1).getDay();
-    let monthHTML =  document.getElementById("month")
+    let monthHTML = document.getElementById("month")
     monthHTML.innerHTML = months[month];
-    monthHTML.innerHTML+= " " + year;
-   
-
-    function addWeeks(){
-       let daysInMonth = new Date(year, month, 0).getDate();
-       let week = `<div class="week-row-1">&nbsp;</div>`
-       let calendar =   document.getElementsByClassName("calendar");
-      calendar.innerHTML+= week;
-    let emptyDays= "<div class='day-contain'>&nbsp;</div>"
-       document.getElementsByClassName("week-row-1").innerHTML += emptyDays.repeat(findFirstDay);
-       let countWeeks = 1;
-       let countDays = 1;
-       for( let i = findFirstDay; i < daysInMonth + findFirstDay; i++ ){   
-        if( i % 7 == 1){
+    monthHTML.innerHTML += " " + year;
+    let daysInMonth = new Date(year, month, 0).getDate();
+    let emptyDays = `<div class='day-contain'>&nbsp;&nbsp;</div>`
+    document.getElementById(`week-row-1`).innerHTML += emptyDays.repeat(findFirstDay);
+    let countWeeks = 1;
+    let countDays = 1;
+    for (let i = findFirstDay + 1; i < daysInMonth + findFirstDay + 1; i++) {
+        let week = document.getElementById(`week-row-${countWeeks}`)
+        if (i % 7 == 0) {
             countWeeks++;
-            calendar.innerHTML+= `<div class="week-row-${countWeeks}"></div>`;
+            week.innerHTML +=
+                `<div class='day-contain edge-day'>${countDays}</div>`
+        } else if (
+            i % 7 == 1
+        ) {
+            week.innerHTML +=
+                `<div class='day-contain edge-day'>${countDays}</div>`
+        } else {
+            week.innerHTML +=
+                `<div class='day-contain'>${countDays}</div>`
         }
-        else{
-            document.getElementsByClassName(`week-row-${countWeeks}`).innerHTML+= 
-            `<div class='day-contain'>${countDays}</div>`
-        }
+
+
         countDays++;
-       }
     }
-
-
-    addWeeks()
 }
